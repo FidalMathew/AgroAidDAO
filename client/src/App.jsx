f6b600e2ff8171a0b06ef23e4f111f769b59f391
Jaydeep <jaydeep.dey03@gmail.com>
2023-05-23 18:44:02 +0530

    migrated to vite

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Text, Box } from '@chakra-ui/react'
+function App() {
+
+  return (
+    <Box minH="100vh">
+      <Text>Hello</Text>
+    </Box>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  initialColorMode: "system",
+  useSystemColorMode: false,
+  colors: {
+    light: {},
+    dark: {}
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    </ChakraProvider>
+  </BrowserRouter>
+)
