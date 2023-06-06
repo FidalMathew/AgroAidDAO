7e466577eb131eb51884cca490821e685ea0c83c
Fidal <fidal15perfect@gmail.com>
2023-06-07 00:18:21 +0530

    usd-eth request contract

--- blockchain/contracts/Request.sol
+++ blockchain/contracts/Request.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * Request testnet LINK and ETH here: https://faucets.chain.link/
@@ -33,21 +35,25 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
      * jobId: ca98366cc7314957b8c012c72f05aeeb
      *
      */
+    AggregatorV3Interface internal dataFeed;
+
     constructor() ConfirmedOwner(msg.sender) {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0x40193c8518BB267228Fc409a613bDbD8eC5a97b3);
         jobId = "ca98366cc7314957b8c012c72f05aeeb";
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+        dataFeed = AggregatorV3Interface(
+            0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada // polygon
+        );
     }
 
     /**
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestVolumeData(
-        string memory _longitude,
-        string memory _latitude
-    ) public returns (bytes32 requestId) {
+    int public ans;
+
+    function requestVolumeData() public returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(
             jobId,
             address(this),
@@ -56,19 +62,23 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
 
         // Set the URL to perform the GET request on
         // Set the URL to perform the GET request on
+        ans = getLatestData();
+
+        string memory amount = Strings.toString(ans);
         string memory url = string(
             abi.encodePacked(
-                "https://api.open-meteo.com/v1/forecast?latitude=",
-                _latitude,
-                "&longitude=",
-                _longitude,
-                "&current_weather=true"
+                "https://api.api-ninjas.com/v1/convertcurrency?want=INR",
+                "&have=USD",
+                "&amount=",
+                amount
             )
         );
 
         req.add("get", url);
 
-        req.add("path", "current_weather,temperature"); // Chainlink nodes 1.0.0 and later support this format
+        req.add("path", "new_amount"); // Chainlink nodes 1.0.0 and later support this format
+
+        req.add("X-Api-Key", "rEAsrWsBXFRiqXLWQM9C5w==HX6ULpUlpQXYd5YM");
 
         // Multiply the result by 1000000000000000000 to remove decimals
         int256 timesAmount = 10 ** 18;
@@ -76,6 +86,25 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
 
         // Sends the request
         return sendChainlinkRequest(req, fee);
+
+        // Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        // request.add("get", 'https://api.api-ninjas.com/v1/convertcurrency?want=INR&have=USD&amount=5000');
+        // request.add("path", "new_amount");
+
+        // // Set the request headers
+        // request.add("X-Api-Key", 'rEAsrWsBXFRiqXLWQM9C5w==HX6ULpUlpQXYd5YM');
+
+        // int256 timesAmount = 10 ** 18;
+        // request.addInt("times", timesAmount);
+
+        // // Sends the request
+        // return sendChainlinkRequestTo(request, fee);
+    }
+
+    bool public dataReceived;
+
+    function getEquivalent() public view returns (uint256) {
+        return volume;
     }
 
     /**
@@ -87,6 +116,7 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
     ) public recordChainlinkFulfillment(_requestId) {
         emit RequestVolume(_requestId, _volume);
         volume = _volume;
+        dataReceived = true;
     }
 
     /**
@@ -100,8 +130,20 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
         );
     }
 
-    function viewTemperature() public view returns (uint256) {
+    function viewConversion() public view returns (uint256) {
         // requestVolumeData();
         return volume;
+    }
+
+    function getLatestData() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /* uint startedAt */,
+            /* uint timeStamp */,
+            /* uint80 answeredInRound */
+        ) = dataFeed.latestRoundData();
+        return answer;
     }
 }
